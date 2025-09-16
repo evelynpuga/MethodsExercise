6508754f0ccff16b179b2cadf2d89362b0c60f1f
Evelyn Puga <pugaevelyn0@gmail.com>
2025-09-16 14:03:58 -0700

    Completed C# story on program exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Prompt the user for their name
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            
+            //Prompt the user for favorite color
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            
+            // Prompt the user for their favorite animal
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            
+            //Prompt the user for their favorite food
+            Console.WriteLine("What is your favorite food?");
+            string food = Console.ReadLine();
+            
+            Console.WriteLine($"I have a pet {animal} named {name}. They have a {color} collar. When they are really hungry, I feed them {food}.");
+            
         }
     }
 }
