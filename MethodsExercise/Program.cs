43ed45b210ef5477ea158a1c2432841101c05315
Evelyn Puga <pugaevelyn0@gmail.com>
2025-09-16 14:32:08 -0700

    re-did the exercise. Created it's own method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,26 +2,30 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void MadLib()
         {
             // Prompt the user for their name
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
+            Console.WriteLine("What is your name?"); //output
+            string name = Console.ReadLine(); //input
             
             //Prompt the user for favorite color
-            Console.WriteLine("What is your favorite color?");
-            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?"); //output
+            string color = Console.ReadLine(); //input
             
             // Prompt the user for their favorite animal
-            Console.WriteLine("What is your favorite animal?");
-            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?"); //output
+            string animal = Console.ReadLine(); //input
             
             //Prompt the user for their favorite food
-            Console.WriteLine("What is your favorite food?");
-            string food = Console.ReadLine();
+            Console.WriteLine("What is your favorite food?"); //output
+            string food = Console.ReadLine();//input
             
+            //story read out
             Console.WriteLine($"I have a pet {animal} named {name}. They have a {color} collar. When they are really hungry, I feed them {food}.");
-            
+        }
+        static void Main(string[] args)
+        {
+           MadLib();
         }
     }
 }
